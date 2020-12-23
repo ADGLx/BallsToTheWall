795cdf51ec6cc67ea00bc07c759a84a145bf638a
Alvaro Gonzalez <alvarodavidglx@hotmail.com>
2020-12-23 18:08:47 -0500

    Added a Simple menu and a loading screen

---
+++ Assets/Scripts/Main_Menu/MainMenuButtons.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine.SceneManagement;
+using UnityEngine;
+
+public class MainMenuButtons : MonoBehaviour
+{
+
+    public void PlayButton()
+    {
+        SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex + 1); //it loads the next scene
+    }
+
+
+    public void ExitGame()
+    {
+        Application.Quit();
+    }
+}

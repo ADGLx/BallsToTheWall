88cd86638e3eadf1dd4999351770c230e0c48c64
ChenBest <lichen9232@gmail.com>
2021-01-01 21:30:36 -0500

    improved boosters by storing them to prefab

--- Assets/Scripts/Bricks/brickManager.cs
+++ Assets/Scripts/Bricks/brickManager.cs
@@ -3,16 +3,9 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using Mirror;
-public class brickManager : NetworkBehaviour
+public class BrickManager : NetworkBehaviour
 {
-    public Type t;
-    private BaseBooster baseBooster;
-    public BoostersTypes m_booster = BoostersTypes.None;
-    // Start is called before the first frame update
-    void Awake()
-    {
-        InitializeBoosterType();
-    }
+    public BaseBooster baseBooster;
 
     private void OnCollisionEnter2D(Collision2D collision)
     {
@@ -33,7 +26,7 @@ public class brickManager : NetworkBehaviour
         {
 
                 //do destroy just set as inactive
-                gameObject.SetActive(true);
+                gameObject.SetActive(false);
 
                 // Trigger any animations
 
@@ -43,30 +36,6 @@ public class brickManager : NetworkBehaviour
         }
         
     }
-
-    //Pairs the enums with the actual class method, I dont like the way it works but unity does not support the drag and drop in the inspector)
-    void InitializeBoosterType()
-    {
-        switch(m_booster)
-        {
-            case BoostersTypes.SpeedBoost:
-                baseBooster = new SpeedBooster();
-                break;
-
-            case BoostersTypes.LengthBoost:
-                baseBooster = new LengthBooster();
-                break;
-            
-            case BoostersTypes.None:
-                baseBooster = null;
-                break;
-
-            default:
-                Debug.LogWarning("Remember to add a case in this switch!");
-                baseBooster = null;
-                break;
-        }
-    }
   
     [Server]
     void CmdApplyBooster(Padel p)
@@ -74,5 +43,3 @@ public class brickManager : NetworkBehaviour
         baseBooster.ApplyBooster(p);
     }
 }
-
-public enum BoostersTypes {SpeedBoost, LengthBoost, None };

97184322b6d5f61d88b2680176555009b8ea651f
Alvaro Gonzalez <alvarodavidglx@hotmail.com>
2021-02-04 01:06:57 -0500

    Updated some UI on the Main Menu

--- Assets/Scripts/Main_Menu/MainMenuButtons.cs
+++ Assets/Scripts/Main_Menu/MainMenuButtons.cs
@@ -2,12 +2,17 @@
 using System.Collections.Generic;
 using UnityEngine.SceneManagement;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class MainMenuButtons : MonoBehaviour
 {
+    [SerializeField] InputField inputField;
+    [SerializeField] Button JoinButton;
 
+    //Add here the way to join the ui
     public void PlayButton()
     {
+        JoinButton.enabled = false;
         SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex + 1); //it loads the next scene
     }
 

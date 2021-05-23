8c2bc21adbbc924e62676174eb24ca833c20588d
Alvaro Gonzalez <alvarodavidglx@hotmail.com>
2021-05-23 19:35:25 -0400

    Added controller for android

--- Assets/Scripts/Android_Controller.cs
+++ Assets/Scripts/Android_Controller.cs
@@ -4,24 +4,54 @@ using UnityEngine;
 
 public class Android_Controller : MonoBehaviour
 {
-#if UNITY_ANDROID
+   #if UNITY_ANDROID
 
+    private float ScreenW;
+    SimpleMovement localPlayer;
 
+    public float MobileMovementSpeed = 0;
 
-    public void MoveRight()
-    {
-        Debug.Log("Moving rIGHT");
-    }
     // Start is called before the first frame update
     void Start()
     {
-        
+        ScreenW = Screen.width;
     }
 
     // Update is called once per frame
-    void Update()
+    void FixedUpdate()
     {
-        
+        if (localPlayer == null)
+        {
+            if (GameObject.FindGameObjectWithTag("Player").GetComponent<SimpleMovement>())
+                localPlayer = GameObject.FindGameObjectWithTag("Player").GetComponent<SimpleMovement>();
+            else
+                Debug.LogError("Player could not be found");
+        } else
+        {
+            int i = 0;
+
+            while (i < Input.touchCount)
+            {
+                if(Input.touchCount == 2)
+                {
+                    localPlayer.GetComponent<Padel>().FireBall();
+                }
+
+                if (Input.GetTouch(i).position.x > ScreenW / 2)
+                {
+                    //MoveRight
+                    localPlayer.HandleMoveHorizontal(1f);
+                }
+
+                if (Input.GetTouch(i).position.x < ScreenW / 2)
+                {
+                    //MoveLeft
+                    localPlayer.HandleMoveHorizontal(-1f);
+                }
+                i++;
+            }
+        }
+
     }
 #endif
 }
